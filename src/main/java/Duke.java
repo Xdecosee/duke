fc15208041d603e19c9000a0e4a60064924622f7
Xdecosee <finalfantasy_1998@hotmail.com>
2019-08-26 11:53:11 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,19 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner echotext = new Scanner(System.in);
+        System.out.println(logo);
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+
+        String input = echotext.nextLine();
+        while(!input.equals("bye")){
+
+            System.out.println(input);
+            input = echotext.nextLine();
+        }
+
+        System.out.println("Bye. Hope to see you again soon!");
+
     }
 }
