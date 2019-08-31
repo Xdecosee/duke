e8532f4341723685691244298bb73689d54e18fe
Xdecosee <finalfantasy_1998@hotmail.com>
2019-08-31 22:36:06 +0800

    Level 5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,38 +15,45 @@ public class Duke {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
 
+        DukeException error = new DukeException();
+
         //Init Input
         ArrayList<Task> inputlist = new ArrayList<Task>();
         String input = echotext.nextLine();
         String command;
 
-        while(!input.equals("bye")){
+        while (!input.equals("bye")) {
 
             //Extract command from first word
             String[] breakdown = input.split("\\s+");
             command = breakdown[0];
 
 
-            if(command.equals("todo")){
+            if (command.equals("todo")) {
+
 
                 try {
-                    String desc = input.substring(5);
-                    //if(desc.length() == 0){
 
-                       // #System.out.println("☹ OOPS!!! The description of a todo cannot be empty.");
-                       // continue;
-                    //}
+                    String desc = input.substring(5);
+
+                    if (desc.isBlank()) {
+
+                        error.emptyDesc(command);
+                        input = echotext.nextLine();
+                        continue;
+                    }
                     Task newtodo = new Todo(desc);
                     inputlist.add(newtodo);
                     System.out.println(newtodo.connect());
                     System.out.println("Now you have " + inputlist.size() + " tasks in the list.");
-                }catch(Exception e){
 
-                    System.out.println("☹ OOPS!!! The format of your input is wrong. Please type 'todo <desc> ' (e.g. todo buy book");
+                } catch (Exception e) {
+
+                    error.emptyDesc(command);
+
                 }
 
-            }
-            else if(command.equals("list")){
+            } else if (command.equals("list")) {
 
                 System.out.println("Here are the tasks in your list:");
 
@@ -56,59 +63,93 @@ public class Duke {
                     System.out.println(index + p.connect());
                 }
 
-            }
-            else if(command.equals("deadline")){
-                try {
-                String[] deadbreak = input.split("\\s+/by\\s+");
-                String desc = deadbreak[0];
-                String by = deadbreak[1];
-                Task newdeadline = new Deadline(desc, by);
-                inputlist.add(newdeadline);
-                System.out.println(newdeadline.connect());
-                System.out.println("Now you have " + inputlist.size() + " tasks in the list.");
-                }catch(Exception e){
+            } else if (command.equals("deadline")) {
 
-                    System.out.println("☹ OOPS!!! The format of your input is wrong. Please type 'deadline <desc> /by <date/time>' (e.g. deadline return book /by Sunday)");
+                try {
+
+                    String[] deadbreak = input.split("\\s+/by\\s+");
+                    String desc = deadbreak[0];
+                    String by = deadbreak[1];
+
+                    if (desc.isBlank() && by.isBlank()) {
+
+                        error.emptyDesc(command);
+                        input = echotext.nextLine();
+                        continue;
+                    }
+
+                    Task newdeadline = new Deadline(desc, by);
+                    inputlist.add(newdeadline);
+                    System.out.println(newdeadline.connect());
+                    System.out.println("Now you have " + inputlist.size() + " tasks in the list.");
+
+                } catch (Exception e) {
+
+                    error.wrongFormat(command);
+
                 }
 
 
-            }
-            else if(command.equals("event")){
+            } else if (command.equals("event")) {
 
                 try {
-                String[] eventbreak = input.split("\\s+/at\\s+");
-                String desc = eventbreak[0];
-                String at = eventbreak[1];
-                Task newdeadline = new Deadline(desc, at);
-                inputlist.add(newdeadline);
-                System.out.println(newdeadline.connect());
-                System.out.println("Now you have " + inputlist.size() + " tasks in the list.");
-                }catch(Exception e){
 
-                    System.out.println("☹ OOPS!!! The format of your input is wrong. Please type 'event <desc> /at <date/time>' (e.g. event project meeting /at Mon 2-4pm)");
+                    String[] eventbreak = input.split("\\s+/at\\s+");
+                    String desc = eventbreak[0];
+                    String at = eventbreak[1];
+
+
+                    if (desc.isBlank() && at.isBlank()) {
+
+                        error.emptyDesc(command);
+                        input = echotext.nextLine();
+                        continue;
+                    }
+
+                    Task newdeadline = new Deadline(desc, at);
+                    inputlist.add(newdeadline);
+                    System.out.println(newdeadline.connect());
+                    System.out.println("Now you have " + inputlist.size() + " tasks in the list.");
+
+                } catch (Exception e) {
+
+                    error.wrongFormat(command);
+
                 }
-            }
-            else if(command.equals("done")){
+
+            } else if (command.equals("done")) {
+
 
                 try {
                     String complete = input.substring(5);
                     Integer index = Integer.valueOf(complete);
-                    inputlist.get(index-1).setDone(true);
-                    String done = inputlist.get(index-1).getStatusIcon();
+
+                    try {
+
+                        inputlist.get(index - 1).setDone(true);
+
+                    } catch (Exception e) {
+
+                        error.outOfBounds();
+                        input = echotext.nextLine();
+                        continue;
+
+                    }
+
+                    String done = inputlist.get(index - 1).getStatusIcon();
                     System.out.println("Nice! I've marked this task as done: ");
-                    System.out.println(" [" + done + "] " + inputlist.get(index-1).getDescription());
+                    System.out.println(" [" + done + "] " + inputlist.get(index - 1).getDescription());
 
-                }catch(Exception e){
+                } catch (Exception e) {
 
-                System.out.println("☹ OOPS!!! The format of your input is wrong. Please type 'done <index of task>' (e.g. done 4) ");
+                    error.wrongFormat(command);
+
+                }
+
+            } else {
+
+                error.wrongCommand();
             }
-
-            }
-            else{
-
-                System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
-            }
-
 
 
             input = echotext.nextLine();
